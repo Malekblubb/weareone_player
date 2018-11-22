c1e52b8334a6f62df6ab440ac99cd0499ebb5d1a
Christoph Malek <malekchristoph@gmail.com>
2018-11-22 11:55:02 +0100

    design

--- streams.hpp
+++ streams.hpp
@@ -3,34 +3,46 @@
 
 #include <string>
 
-namespace Streams {
-enum Stream : int {
-    technobase = 0,
-    hardbase,
-    trancebase,
-    coretime,
-    clubtime,
-    housetime
-};
+namespace Streams
+{
+	enum Stream : int
+	{
+		technobase = 0,
+		hardbase,
+		trancebase,
+		coretime,
+		clubtime,
+		housetime
+	};
 
-inline std::string getUrlFromStreamId(int s) {
-    switch(s) {
-    case Stream::technobase: return "technobase.fm/";
-        break;
-    case Stream::hardbase: return "hardbase.fm/";
-        break;
-    case Stream::trancebase: return "trancebase.fm/";
-        break;
-    case Stream::coretime: return "coretime.fm/";
-        break;
-    case Stream::clubtime: return "clubtime.fm/";
-        break;
-    case Stream::housetime: return "housetime.fm/";
-        break;
-    default: break;
+	inline std::string getUrlFromStreamId(int s)
+	{
+		std::string ret = "";
+		switch(s)
+		{
+			case Stream::technobase:
+				ret = "technobase.fm/";
+				break;
+			case Stream::hardbase:
+				ret = "hardbase.fm/";
+				break;
+			case Stream::trancebase:
+				ret = "trancebase.fm/";
+				break;
+			case Stream::coretime:
+				ret = "coretime.fm/";
+				break;
+			case Stream::clubtime:
+				ret = "clubtime.fm/";
+				break;
+			case Stream::housetime:
+				ret = "housetime.fm/";
+				break;
+			default:
+				break;
+		}
+		return ret;
     }
-    return "";
-}
-}
+}// namespace Streams
 
-#endif // STREAMS_HPP
+#endif// STREAMS_HPP
